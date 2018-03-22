19479dc045f34fba593d449f14fc33bc2d88698f
Lisa <lye@missouriwestern.edu>
2018-03-21 19:06:31 -0500

    Push update.

---
+++ src/app/layout/content/sidenav/sidenav.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-sidenav',
+  templateUrl: './sidenav.component.html',
+  styleUrls: ['./sidenav.component.css']
+})
+export class SidenavComponent implements OnInit {
+  filters = [
+    {value: 'printer-0', viewValue: 'Printers'},
+    {value: 'keyboard-1', viewValue: 'Laptops'},
+    {value: 'tablet-2', viewValue: 'Tablet'},
+    // {value: 'pc-3', viewValue: 'Network Switches'},
+    // {value: 'fire-4', viewValue: 'Fire Suppression'},
+    // {value: 'fiber-5', viewValue: 'Fibers'},
+    // {value: 'phone-6', viewValue: 'Phones'},
+    // {value: 'backup-5', viewValue: 'Backup Servers'},
+    // {value: 'physical-5', viewValue: 'Physical Servers'},
+  ];
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
